8dc00403a708332b683df6621da0676b62087787
Samuel Yoo <samuel.yoo@vizientinc.com>
2025-01-08 15:49:29 -0600

    added dummy card data and started supported storing card data in HandPanelComponent

---
+++ src/app/dummy-cards.ts
@@ -0,0 +1,14 @@
+export const DUMMY_CARDS = [
+    {
+        id: 'DMU107',
+        name: "Sheoldred, the Apocalypse",
+        cardArt: "assets/card/shelly2.png",
+        cardType: "reinforce"
+    },
+    {
+        id: 'BOLT',
+        name: "Lightning Bolt",
+        cardArt: "assets/card/bolt.png",
+        cardType: "attackCommand"
+    },
+]
--- src/app/hand-panel/hand-panel.component.ts
+++ src/app/hand-panel/hand-panel.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CardComponent } from './card/card.component';
+import { DUMMY_CARDS } from '../dummy-cards';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_CARDS.length);
 
 @Component({
   selector: 'app-hand-panel',
@@ -8,5 +11,5 @@ import { CardComponent } from './card/card.component';
   styleUrl: './hand-panel.component.css'
 })
 export class HandPanelComponent {
-
+  cardsInHand = //creating new property contained in the HandPanelComponent
 }
