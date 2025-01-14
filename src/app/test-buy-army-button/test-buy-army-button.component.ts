969c8b2901122d76613198bcf8628c131235f128
T1-syoo <samuel.yoo@vizientinc.com>
2025-01-14 16:00:30 -0600

    added click button functionality to test buy army button; not actually updating game state so just updates within component state for experimentation purposes

--- src/app/test-buy-army-button/test-buy-army-button.component.ts
+++ src/app/test-buy-army-button/test-buy-army-button.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { pResources } from '../game-state-data/pResources';
 
 @Component({
   selector: 'app-test-buy-army-button',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './test-buy-army-button.component.css'
 })
 export class TestBuyArmyButtonComponent {
-
+  goldAvailableRef = pResources.p1.currency.goldAmount;
+  notEnoughGoldError = ""
+  buyArmyOnClick() {
+    console.log("Clicked!"); 
+    if (this.goldAvailableRef < 50) {
+      this.notEnoughGoldError = "Not enough gold! (50 gold required)" //not getting updated in the UI
+      console.log("not enough gold error triggered")
+    } else {
+      this.goldAvailableRef = this.goldAvailableRef - 50; //not getting updated in the UI
+      this.notEnoughGoldError = "";
+      console.log("gold now left:" + this.goldAvailableRef);
+    }
+  }
+  //only updating this component's state, but it's not displaying anything with data within the component
+  //this. is probably only within this component instance and not updating anything in the stored object data
+  //not tracking across components, if this was all within the resource pool component i think it would work
 }
